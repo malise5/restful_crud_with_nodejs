7274258e5750e4a4bf2f84603c6e5fb5437f9e08
Halkano Malise <kudezx@gmail.com>
2023-04-07 12:43:14 +0300

    update

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 const app = express();
 const port = 8000;
+
+// '============Database Connection=======================
+
+mongoose
+  .connect(
+    "mongodb+srv://malise:malise@cluster1.ulbt2t8.mongodb.net/Node-Api?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("====================================");
+    console.log("Connection to the Database is established");
+    console.log("====================================");
+
+    // '============Server Port=======================
+    app.listen(port, () => {
+      console.log("====================================");
+      console.log("Server Listening on port " + port);
+      console.log("====================================");
+    });
+    // =============Server Port=======================
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// '============Database Connection=======================
 
 //ROUTES
 const router = express.Router();
@@ -10,11 +34,3 @@ const router = express.Router();
 app.get("/api/v1/", (req, res) => {
   res.send("Hallo world");
 });
-
-// '============Server Port=======================
-app.listen(port, () => {
-  console.log("====================================");
-  console.log("Server Listening on port " + port);
-  console.log("====================================");
-});
-// =============Server Port=======================
